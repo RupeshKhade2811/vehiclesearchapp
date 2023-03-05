3129baec933061c6306b92f64fa8a831d2a6640b
Rupesh Khade <rupeshkhade7387@gmail.com>
2023-03-05 13:39:50 +0530

    vehiclePicture1 added

--- src/main/java/com/factory/appraisal/vehiclesearchapp/dto/AppraisalTestDRStsImages.java
+++ src/main/java/com/factory/appraisal/vehiclesearchapp/dto/AppraisalTestDRStsImages.java
@@ -7,22 +7,22 @@ import lombok.Setter;
 
 @NoArgsConstructor
 public class AppraisalTestDRStsImages {
-    private String rearRightImage;
-    public String getRearRightImage() {
-        return rearRightImage;
-    }
-
-    public void setRearRightImage(String rearRightImage) {
-        this.rearRightImage = rearRightImage;
-    }
-
-//    private String vehiclePicture1;
-//
-//    public String getVehiclePicture1() {
-//        return vehiclePicture1;
+//    private String rearRightImage;
+//    public String getRearRightImage() {
+//        return rearRightImage;
 //    }
 //
-//    public void setVehiclePicture1(String vehiclePicture1) {
-//        this.vehiclePicture1 = vehiclePicture1;
+//    public void setRearRightImage(String rearRightImage) {
+//        this.rearRightImage = rearRightImage;
 //    }
+
+    private String vehiclePicture1;
+
+    public String getVehiclePicture1() {
+        return vehiclePicture1;
+    }
+
+    public void setVehiclePicture1(String vehiclePicture1) {
+        this.vehiclePicture1 = vehiclePicture1;
+    }
 }
